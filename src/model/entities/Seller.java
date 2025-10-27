5d9bac048a10c5eb79b01a5a109d79637ee22f32
Weverton Mathias Rocha <Wevertonmathias01@gmail.com>
2025-10-27 16:59:48 -0300

    update inplementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -7,25 +7,25 @@ import java.util.Objects;
 public class Seller implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
-	
-	public Integer id;
-	public String name;
-	public String email;
-	public Date birthDate;
-	public Double baseSalary;
+
+	private Integer id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private Double baseSalary;
 	
 	private Department department;
 	
-	public Seller(Object object, String string, String string2, java.sql.Date date, double d, Department department2) {
+	public Seller() {
 	}
 
-	public Seller() {
-		
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.birthDate = birthDate;
 		this.baseSalary = baseSalary;
+		this.department = department;
 	}
 
 	public Integer getId() {
@@ -78,7 +78,10 @@ public class Seller implements Serializable{
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(baseSalary);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -90,7 +93,12 @@ public class Seller implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Seller other = (Seller) obj;
-		return Objects.equals(baseSalary, other.baseSalary);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	@Override
@@ -98,5 +106,4 @@ public class Seller implements Serializable{
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
-	
 }
